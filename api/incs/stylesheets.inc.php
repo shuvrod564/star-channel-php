f771fa1e336b073e00bea87782480a1bfc174eee
shuvrod564 <shuvrod564@gmail.com>
2026-01-06 19:19:06 +0600

    url

--- api/incs/stylesheets.inc.php
+++ api/incs/stylesheets.inc.php
@@ -2,9 +2,9 @@
 
 <?php if ($folder === 'root') { ?>
     <!-- CSS: All Vendors -->
-    <link rel="stylesheet" href="css/vendor.css">   
+    <link rel="stylesheet" href="./css/vendor.css">   
     <!-- CSS: Html Designer -->
-    <link rel="stylesheet" href="css/style.css">  
+    <link rel="stylesheet" href="./css/style.css">  
 <?php } if ($folder === 'blog') { ?>
     <!-- CSS: All Vendors -->
     <link rel="stylesheet" href="../css/vendor.css">   
